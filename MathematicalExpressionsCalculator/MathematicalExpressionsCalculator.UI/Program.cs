620b0521c3fbe682f3c548e910a8e7f8e0275d56
geoGitAm <71641416+geoGitAm@users.noreply.github.com>
2021-02-08 01:16:29 +0300

    initial commit.

---
+++ MathematicalExpressionsCalculator/MathematicalExpressionsCalculator.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathematicalExpressionsCalculator.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
